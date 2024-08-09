41376678638851b9d77839ee655b0dac2282cbf2
ms2548 <guminsun9@gmail.com>
2024-08-09 11:15:41 +0900

    0809_전체 옮기기

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ViewMemoriesPagePc from './screen/pc/ViewMemoriesPagePc'; // 경로가 정확한지 확인하세요
+import { BrowserRouter as Router } from 'react-router-dom'; // Router 임포트
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+   <Router>
+      <ViewMemoriesPagePc />  </Router>
+   
   </React.StrictMode>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
